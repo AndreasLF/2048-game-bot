c1ed82f3c296dc84773343d231f108a1c69ab936
Andreas Fiehn <andreas@lyhnefiehn.dk>
2020-09-27 19:09:05 +0200

    Feat: Loop through the game and save final score to list

--- AI_Game2048.py
+++ AI_Game2048.py
@@ -13,72 +13,87 @@ from Game2048 import Game2048
 import numpy as np
 import pygame
 
-env = Game2048()
-env.reset()
-actions = ['left', 'right', 'up', 'down']
-exit_program = False
-action_taken = False
-while not exit_program:
-    env.render()
+# env = Game2048()
+# env.reset()
+# actions = ['left', 'right', 'up', 'down']
+# exit_program = False
+# action_taken = False
 
-    # Process game events
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            exit_program = True
-        if event.type == pygame.KEYDOWN:
-            if event.key in [pygame.K_ESCAPE, pygame.K_q]:
+saved_scores = []
+amount_of_simulations = 2
+number_of_tests = 5
+
+for i in range(number_of_tests):
+    env = Game2048()
+    env.reset()
+    actions = ['left', 'right', 'up', 'down']
+    exit_program = False
+    action_taken = False
+    while not exit_program:
+        env.render()
+
+        # Process game events
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
                 exit_program = True
-            if event.key == pygame.K_UP:
-                action, action_taken = 'up', True
-            if event.key == pygame.K_DOWN:
-                action, action_taken  = 'down', True
-            if event.key == pygame.K_RIGHT:
-                action, action_taken  = 'right', True
-            if event.key == pygame.K_LEFT:
-                action, action_taken  = 'left', True
-            if event.key == pygame.K_r:
-                env.reset()
+            if event.type == pygame.KEYDOWN:
+                if event.key in [pygame.K_ESCAPE, pygame.K_q]:
+                    exit_program = True
+                if event.key == pygame.K_UP:
+                    action, action_taken = 'up', True
+                if event.key == pygame.K_DOWN:
+                    action, action_taken  = 'down', True
+                if event.key == pygame.K_RIGHT:
+                    action, action_taken  = 'right', True
+                if event.key == pygame.K_LEFT:
+                    action, action_taken  = 'left', True
+                if event.key == pygame.K_r:
+                    env.reset()
 
 
-    # INSERT YOUR CODE HERE
+        # INSERT YOUR CODE HERE
 
-    # Dictionairy to contain scores for each first step
-    scores = {'left': 0, 'right': 0, 'up': 0, 'down': 0}
+        # Dictionairy to contain scores for each first step
+        scores = {'left': 0, 'right': 0, 'up': 0, 'down': 0}
 
-    # Loops through a desired amount of games for each initial move
-    for n in range(20):
         # Loops thorugh possible actions
         for first_action in actions:
-            # A Game2048 object is created with the current state of the game
-            # With this object we will "search" for the best solution for our next move by going randomly through the steps
-            simulation = Game2048((env.board, env.score))
+            # Loops through a desired amount of games for each initial move
+            for n in range(amount_of_simulations):
+                # A Game2048 object is created with the current state of the game
+                # With this object we will "search" for the best solution for our next move by going randomly through the steps
+                simulation = Game2048((env.board, env.score))
 
-            # Takes the first step
-            (board, score), reward, done = simulation.step(first_action)
+                # Takes the first step
+                (board, score), reward, done = simulation.step(first_action)
 
-            # Loops until the simulation hits game over. Done takes the value of game_over in the Game2048 object
-            while not done:
-                # Picks a random action from the actions list
-                action = actions[np.random.randint(4)]
-                # The step is performed
-                (board, score), reward, done = simulation.step(action)
+                # Loops until the simulation hits game over. Done takes the value of game_over in the Game2048 object
+                while not done:
+                    # Picks a random action from the actions list
+                    action = actions[np.random.randint(4)]
+                    # The step is performed
+                    (board, score), reward, done = simulation.step(action)
 
-                if done:
-                    # Adds the score to the scores dictionairy
-                    scores[first_action] += score
+                    if done:
+                        # Adds the score to the scores dictionairy
+                        scores[first_action] += score
 
-                action_taken = True
+                    action_taken = True
 
-    # Scores are printed
-    print(scores)
-    # Get the key of action in the scores dict with the highest value. This is the action to choose
-    next_action = max(scores, key = lambda k: scores[k])
-    print(next_action)
+        # Scores are printed
+        print(scores)
+        # Get the key of action in the scores dict with the highest value. This is the action to choose
+        next_action = max(scores, key = lambda k: scores[k])
+        print(next_action)
 
-    # END OF YOUR CODE
+        # END OF YOUR CODE
 
-    if action_taken:
-        (board, score), reward, done = env.step(next_action)
-        action_taken = False
+        if action_taken:
+            (board, score), reward, done = env.step(next_action)
+            if done:
+                saved_scores.append(score)
+                print(saved_scores)
+                exit_program = True
+            action_taken = False
 
 env.close()
